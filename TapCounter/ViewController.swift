8f10c79827d1d66b4cfbf672a002a5d67d275c6f
Rimil Dey <rimildeyjsr@gmail.com>
2017-10-19 15:08:34 +0530

    initial and final commit

--- TapCounter/ViewController.swift
+++ TapCounter/ViewController.swift
@@ -10,16 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - View Did Load
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        self.navigationController?.navigationBar.setBackgroundImage(UIImage(), for: UIBarMetrics.default)
+        self.navigationController?.navigationBar.shadowImage = UIImage()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // MARK: - outlets
+    
+    @IBOutlet weak var countLabel: UILabel!
+    
+    // MARK: - Properties
+    var count = 0
+    
+    // MARK: - Interactions
+    @IBAction func longPressTapButton(_ sender: UILongPressGestureRecognizer) {
+        increaseCount()
     }
-
+    
+    @IBAction func tapCounter(_ sender: UIButton) {
+        increaseCount()
+    }
+    
+    @IBAction func tapResetButton(_ sender: UIBarButtonItem) {
+        countLabel.text = "0"
+        count = 0
+    }
+    
+    // MARK: - Functions
+    func increaseCount(){
+        count = count + 1
+        countLabel.text = String(count)
+    }
 
 }
 
